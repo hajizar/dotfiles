688c07679e4b7c0899d11cc182ba05f663ccd9a4
Rufat Hajizada <rufat.hajizada@schrodinger.com>
2024-10-15 11:08:58 -0400

    Updating colorscheme

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,26 @@
 return {
-  { "projekt0n/github-nvim-theme" },
+  {
+    "projekt0n/github-nvim-theme",
+    config = function()
+      require("github-theme").setup({
+        options = {
+          transparent = true,
+          styles = {
+            comments = "italic",
+            keywords = "bold",
+            types = "italic,bold",
+          },
+          inverse = {
+            visual = true,
+          },
+        },
+      })
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "github_dark_high_contrast",
+      colorscheme = "github_dark",
     },
   },
 }
