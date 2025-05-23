5896a72f91eca262f381a078e951c217b78175df
Rufat Hajizada <rufat.hajizada@schrodinger.com>
2025-05-23 13:34:20 -0400

    upading lazyvim, updated placeholder

--- config/nvim/lua/plugins/placeholder.lua
+++ config/nvim/lua/plugins/placeholder.lua
@@ -2,7 +2,38 @@ return {
   {
     "rhajizada/placeholder",
     config = function()
-      require("placeholder").setup()
+      require("placeholder").setup({
+        keymap = "<leader>cj",
+        dap_config_types = {
+          go = {
+            debugger = "go",
+          },
+          python = {
+            debugger = "debugpy",
+          },
+          lua = {
+            debugger = "nlua",
+          },
+          javascript = {
+            debugger = "pwa-node",
+            request = "launch",
+            program = "${file}",
+            cwd = "${workspaceFolder}",
+            console = "integratedTerminal",
+            args = {},
+            env = { NODE_ENV = "development" },
+          },
+          typescript = {
+            debugger = "pwa-node",
+            request = "launch",
+            program = "${file}",
+            cwd = "${workspaceFolder}",
+            console = "integratedTerminal",
+            args = {},
+            env = { NODE_ENV = "development" },
+          },
+        },
+      })
     end,
   },
 }
