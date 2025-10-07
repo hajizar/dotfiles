2115b936663a5f760c3e816b4dcc30d63b9a05ab
Rufat Hajizada <rufat.hajizada@schrodinger.com>
2025-10-07 12:01:35 -0400

    updating lazyvim, replacing codewindow with minimap

---
+++ config/nvim/lua/plugins/minimap.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "Isrothy/neominimap.nvim",
+    version = "v3.x.x",
+    lazy = false,
+    keys = {
+      { "<leader>mm", "<cmd>Neominimap Toggle<cr>", desc = "Toggle global minimap" },
+      { "<leader>mo", "<cmd>Neominimap Enable<cr>", desc = "Enable global minimap" },
+      { "<leader>mc", "<cmd>Neominimap Disable<cr>", desc = "Disable global minimap" },
+    },
+    init = function()
+      vim.opt.wrap = false
+      vim.opt.sidescrolloff = 36
+
+      ---@type Neominimap.UserConfig
+      vim.g.neominimap = {
+        auto_enable = true,
+      }
+    end,
+  },
+}
