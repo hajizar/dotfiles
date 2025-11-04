59b4020cf1955f5309699fa6022fc4c6c42fc0fe
Rufat Hajizada <rufat.hajizada@schrodinger.com>
2025-11-03 21:44:08 -0500

    showing some hidden files in file finder

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -15,6 +15,24 @@ return {
           "__pycache__",
           "node_modules",
         },
+        sources = {
+          files = {
+            cmd = "fd",
+            args = {
+              "--type", "f",
+              "--type", "l",
+              "--color", "never",
+              "--hidden",
+              "--exclude", ".git",
+              "--exclude", ".hypothesis",
+              "--exclude", ".pytest_cache",
+              "--exclude", ".ruff_cache",
+              "--exclude", ".venv",
+              "--exclude", "__pycache__",
+              "--exclude", "node_modules",
+            },
+          },
+        },
       },
       explorer = {
         enabled = true,
